d933debb5c9271c255aea7597953bcdb174365ac
Jonas-Desktop <studies@jonas.zengerle.email>
2024-05-19 23:13:03 +0200

    Hinzufügen der Prozedur SubstancesAreValid zum Validieren von Substanzen mittels des Json-Schemas.

--- Source/Substance/Validator.py
+++ Source/Substance/Validator.py
@@ -1,4 +1,18 @@
 import json
+import jsonschema
 
 def SubstancesAreValid (substances_to_validate) -> bool:
-    return
+    result = False
+
+    with open("C:\\Users\\Studies\\Programmierprojekt\\Source\\Substance\\Schema.json", "r") as schema_file:
+        schema = json.load(schema_file)
+
+        try:
+            jsonschema.validate(instance = substances_to_validate, schema = schema)
+            
+            result = True
+        
+        except:
+            pass # ToDo: Fehlerbehandlung
+
+    return result
