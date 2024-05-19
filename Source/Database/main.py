7d7304ecdf8bda494966ea8c99a82f11ec588d1d
Jonas-Desktop <studies@jonas.zengerle.email>
2024-05-19 20:09:35 +0200

    Anpassen der Datenbank-Schnittstelle so das diese eingehende Json-Dokumente validiert.

--- Source/Database/main.py
+++ Source/Database/main.py
@@ -1,6 +1,14 @@
 import json
 
+from ..Substance import Validator as SubstanceValidator
+
 def SetSubstances (substances):
+    if SubstanceValidator.SubstancesAreValid(substances) == True:
+        pass
+    
+    else:
+        pass
+
     return
 
 def GetSubstances (searchCriteria) -> json:
