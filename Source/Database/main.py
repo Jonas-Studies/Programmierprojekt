0fc57a13624c3904dc111862480a09dd00dfd7bf
Jonas-Desktop <studies@jonas.zengerle.email>
2024-05-20 22:06:55 +0200

    Hinzufügen der SetSubstance Prozedur zum Verwalten einer Substanz in der Datenbank.

--- Source/Database/main.py
+++ Source/Database/main.py
@@ -6,9 +6,38 @@ from Source.Database import MongoDB
 def SetSubstances (substances):
     if SubstanceValidator.SubstancesAreValid(substances) == True:
         pass
-    
+
     else:
         pass
+
+    return
+
+def SetSubstance (substance):
+    Database = MongoDB.Substances()
+
+    searchCriteria = {
+        "source": substance["source"],
+        "deleted": False
+    }
+
+    existingSubstances = Database.GetSubstances(searchCriteria)
+
+    if existingSubstances.__len__ == 0:
+        Database.InsertSubstance(substance)
+    
+    else:
+        if existingSubstances.__len__ == 1:
+            existingSubstance = existingSubstances[0]
+            
+            if existingSubstance["last_modified"] != substance["last_modified"]:
+                Database.UpdateSubstanceByID(existingSubstance["_id"], substance)
+
+            else:
+                # Wenn last_modified unverändert ist, sollte die Substanz in der Datenbank die erhaltene wie die Übergebene aussehen
+                pass
+
+        else:
+            pass # ToDo: Was wenn zu einer Quelle + Smiles mehr als ein Datensatz existiert?
 
     return
 
