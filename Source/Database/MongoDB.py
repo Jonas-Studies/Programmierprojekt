9b464cd1afbfb4019559c73b567e06f0b8773b6e
Jonas-Desktop <studies@jonas.zengerle.email>
2024-05-20 12:55:00 +0200

    Hinzufügen eines Moduls zum Anbinden einer MongoDB als Datenbank. Hinzufügen einer Klasse zum Verarbeiten von Substanzen mit Funktionen zum Anlegen und Laden von Substanzen.

--- Source/Database/MongoDB.py
+++ Source/Database/MongoDB.py
@@ -1,8 +1,35 @@
-import json
+from pymongo import MongoClient
+
+MONGODB_HOSTNAME = "localhost"
+MONGODB_PORT = 27017
+
+DATABASE_NAME = "designerdrugdatabase"
 
 class Substances:
-    def UpsertSubstance (self, substance):
+    COLLECTION_NAME = "substances"
+
+    def __init__(self) -> None:
+        self.connection = MongoClient(MONGODB_HOSTNAME, MONGODB_PORT)
+        self.collection = self.connection[DATABASE_NAME][self.COLLECTION_NAME]
+
         return
     
-    def GetSubstances (self, searchCriteria) -> json:
+    def __del__(self) -> None:
+        self.connection.close()
+
         return
+
+    def InsertSubstance (self, substance) -> None:
+        self.collection.insert_one(substance)
+
+        return
+    
+    def GetSubstances (self, searchCriteria) -> list:
+        substances = self.collection.find(searchCriteria)
+
+        result = []
+
+        for substance in substances:
+            result.append(substance)
+
+        return result
