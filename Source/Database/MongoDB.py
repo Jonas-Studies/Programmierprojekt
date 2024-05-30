e54dcf8042b1e9282828ea2390e780290977c347
Jonas-Desktop <studies@jonas.zengerle.email>
2024-05-31 01:24:03 +0200

    Hinzufügen von Funktionen für insert und update mehrerer Substanzen zum MongoDB Modul

--- Source/Database/MongoDB.py
+++ Source/Database/MongoDB.py
@@ -12,17 +12,38 @@ class Substances:
         self.connection = MongoClient(MONGODB_HOSTNAME, MONGODB_PORT)
         self.collection = self.connection[DATABASE_NAME][self.COLLECTION_NAME]
 
-        return
+        return None
     
     def __del__(self) -> None:
         self.connection.close()
 
-        return
+        return None
 
     def insert_substance (self, substance) -> None:
         self.collection.insert_one(substance)
 
-        return
+        return None
+
+    def insert_substances (self, substances) -> None:
+        self.collection.insert_many(substances)
+
+        return None
+    
+    def update_substance (self, substance) -> None:
+        self.collection.replace_one(
+            filter = {
+                "_id": substance['_id']
+            },
+            replacement = substance
+        )
+
+        return None
+
+    def update_substances (self, substances) -> None:
+        for substance in substances:
+            self.update_substance(substance)
+
+        return None
     
     def update_substance_by_id(self, id, substance) -> None:
         searchCriterias = {
@@ -31,9 +52,9 @@ class Substances:
 
         self.collection.replace_one(searchCriterias, substance)
 
-        return
+        return None
     
-    def get_substances (self, searchCriteria) -> list:
+    def get_substances (self, searchCriteria) -> list[dict]:
         substances = self.collection.find(searchCriteria)
 
         result = []
