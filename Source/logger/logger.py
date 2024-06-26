058a596503c3e9bc99a873b301a236125c30ab2c
RDust4 <jojopiermeier7@gmail.com>
2024-06-26 10:18:49 +0200

    Hinzufügen eines Loggers

---
+++ Source/logger/logger.py
@@ -0,0 +1,45 @@
+import logging
+import os
+
+LOGGING_FILE = './logs/log.log'
+LOGGING_LEVEL = logging.DEBUG
+
+
+# Loggers for each module
+loggers = {}
+
+
+def _get_logger(module_name):
+    if module_name in loggers:
+        return loggers[module_name]
+    
+    os.makedirs(os.path.dirname(LOGGING_FILE), exist_ok=True)
+    logging.basicConfig(
+        format='%(asctime)s [%(levelname)-8s] %(name)-15s - %(message)s',
+        filename=LOGGING_FILE,
+        encoding='utf-8',
+        level=LOGGING_LEVEL
+    )
+    
+    logger = logging.getLogger(module_name)
+    logger.setLevel(LOGGING_LEVEL)
+    
+    loggers[module_name] = logger
+    return logger
+    
+
+def debug(module_name, msg):
+    _get_logger(module_name).debug(msg)
+    
+def info(module_name, msg):
+    _get_logger(module_name).info(msg)
+    
+def warning(module_name, msg):
+    _get_logger(module_name).warning(msg)
+    
+def error(module_name, msg):
+    _get_logger(module_name).error(msg)
+    
+def critical(module_name, msg):
+    _get_logger(module_name).critical(msg)
+    
