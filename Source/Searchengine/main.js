b802f016dd17651b63aeda7e15e282de866187ac
Jonas-Laptop <studies@jonas.zengerle.email>
2024-06-27 11:05:55 +0200

    Added a frist test route

--- Source/Searchengine/main.js
+++ Source/Searchengine/main.js
@@ -4,6 +4,15 @@ const express = require("express")
 const app = express()
 const PORT = 3000
 
+app.use(
+    '/',
+    function (request, response, next) {
+        console.info('Recieved request to display the search page')
+
+        response.status(200).send('Moin')
+    }
+)
+
 app.listen(
     PORT,
     function () {
