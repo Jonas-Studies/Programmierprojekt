3838b3732d23b1de319c23b3a947034921fb07f6
Jonas-Laptop <studies@jonas.zengerle.email>
2024-06-27 17:37:30 +0200

    Hinzufügen einer Route für die Suchergebnis Seite

--- Source/Searchengine/main.js
+++ Source/Searchengine/main.js
@@ -10,11 +10,29 @@ app.set('view engine', 'pug')
 app.use(express.static('./public'))
 
 app.use(
-    '/',
+    '/search',
     function (request, response, next) {
         console.info('Recieved request to display the search page')
 
         response.status(200).render('search_page')
+    }
+)
+
+app.use(
+    '/searchresults',
+    function (request, response, next) {
+        console.info('Recieved request to display the search-results page')
+
+        response.status(200).render('searchresults_page')
+    }
+)
+
+app.use(
+    '/',
+    function (request, response, next) {
+        console.info('Recieved request to display the main page')
+
+        response.redirect('/search')
     }
 )
 
