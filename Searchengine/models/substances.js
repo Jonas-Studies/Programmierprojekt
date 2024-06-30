5c68ab37b162d19e7b98ed995976736998505301
Jonas-Desktop <studies@jonas.zengerle.email>
2024-06-30 14:24:32 +0200

    Hinzufügen des js datenbank Moduls zu dem Substences Modul des Models

--- Searchengine/models/substances.js
+++ Searchengine/models/substances.js
@@ -1,12 +1,18 @@
-exports.get_many_by_searchArguments = function (searchArguments) {
-    return [
-        {
-            url: 'https://google.com',
-            source: 'Trust me bro'
-        },
-        {
-            url: 'https://google.com',
-            source: 'Trust me bro'
+const { Database_collection } = require('../database.js')
+
+exports.get_many_by_searchCriteria = async function (searchCriteria) {
+    let result = undefined
+
+    const substances_collection = new Database_collection('substances')
+    
+    await substances_collection.with_connection(
+        async (collection) => {
+            result = await collection.find(undefined).toArray()
         }
-    ]
+    )
+
+    console.info('Loaded substances by searchcriteria')
+    console.debug(result)
+
+    return result
 }
