8fd4ee8a84ab99595b5ac44b1c7613f237a1b63f
RDust4 <jojopiermeier7@gmail.com>
2024-07-02 09:36:07 +0200

    Verbessern der Import-Performance

--- Python/import.py
+++ Python/import.py
@@ -1,18 +1,14 @@
 from data_manager import importer
+from settings import FIX_SUBSTANCES
 
-from datetime import datetime
 import os
 
-
-FIX_SUBSTANCES = True
-
-
 if __name__ == "__main__":
-    if os.path.exists("../Imports"):
-        files = os.listdir("../Imports")
+    if os.path.exists("./Imports"):
+        files = sorted(os.listdir("./Imports"))
         for file in files:
             if file.endswith(".json"):
-                importer.import_data_from_file(f"../Imports/{file}", FIX_SUBSTANCES)
+                importer.import_data_from_file(f"./Imports/{file}", FIX_SUBSTANCES)
             
     importer.import_data_from_caymanchem(FIX_SUBSTANCES)
     
--- Python/settings.py
+++ Python/settings.py
@@ -6,4 +6,8 @@ MONGODB_PORT = 27017
 DATABASE_NAME = "designerdrugdatabase"
 
 LOGGING_FILE = './Logs/log.log'
-LOGGING_LEVEL = logging.DEBUG
+LOGGING_LEVEL = logging.INFO
+
+FIX_SUBSTANCES = True
+
+CPU_CORES = 24
