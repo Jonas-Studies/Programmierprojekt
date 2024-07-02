fbc72543d1411de8b8866c88f5bf4111fc463a33
Jonas-Laptop <studies@jonas.zengerle.email>
2024-07-02 12:17:56 +0200

    Anpassen der Erstelung und Verarbeitung der Suchkriterien

--- Searchengine/main.js
+++ Searchengine/main.js
@@ -14,10 +14,28 @@ app.use(
     async function (request, response, next) {
         console.info('Recieved request to display the search page')
 
-        if (request.query.keyphrase != undefined || request.query.min_molecular_mass != undefined && request.query.max_molecular_maxx != undefined) {
+        let keyphrase = undefined
+
+        if (request.query.keyphrase != undefined && request.query.keyphrase != '') {
+            keyphrase = request.query.keyphrase
+        }
+
+        let min_molecular_mass = undefined
+
+        if (request.query.min_molecular_mass != undefined && request.query.min_molecular_mass != '') {
+            min_molecular_mass = Number(request.query.min_molecular_mass)
+        }
+
+        let max_molecular_mass = undefined
+
+        if (request.query.max_molecular_mass != undefined && request.query.max_molecular_mass != '') {
+            max_molecular_mass = Number(request.query.max_molecular_mass)
+        }
+
+        if (keyphrase != undefined || min_molecular_mass != undefined || max_molecular_mass != undefined) {
             const substances_model = require('./models/substances')
     
-            const searchCriteria = substances_model.get_searchCriteria(request.query.keyphrase, request.query.min_molecular_mass, request.query.max_molecular_maxx)
+            const searchCriteria = substances_model.get_searchCriteria(keyphrase, min_molecular_mass, max_molecular_mass)
     
             const substances = await substances_model.get_many_by_searchCriteria(searchCriteria)
     
